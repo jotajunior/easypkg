c9444e41bb6bfa584d6dab936ef43e2779882fda
Jota Junior <jotavrj@gmail.com>
2016-12-28 10:52:53 -0200

    First commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='easypkg',
+        namespace_packages=['easypkg'],
+        packages=['easypkg'],
+        description='Easy downloader of private repositories.',
+        author='Jota Junior',
+        author_email='jotavrj@gmail.com',
+        url='https://github.com/jotajunior/easypkg',
+        download_url='https://github.com/jotajunior/easypkg/tarball/0.1',
+        version='0.1',
+        packages=find_packages('src'),
+        install_requires=[
+            'requests',
+            ],
+        package_dir={'': 'src'},
+        entry_points={
+            'console_scripts': [],
+            },
+        keywords=[
+            'bitbucket',
+            'github',
+            'private',
+            'repository',
+            'download',
+            'repo',
+            ],
+)
